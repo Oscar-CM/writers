3f2792958465bd34883d4c61352c782573aa65aa
Oscar <oscarcheruiyot3@gmail.com>
2025-12-17 22:37:16 +0300

    adding tawk

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,7 +108,7 @@ export default function RootLayout({
             (function(){
               var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
               s1.async=true;
-              s1.src='https://embed.tawk.to/6936dcc884e85e1973f08671/1jbv4srl9';
+              s1.src='https://embed.tawk.to/6943047239e95e197be5143c/1jcmsio35';
               s1.charset='UTF-8';
               s1.setAttribute('crossorigin','*');
               s0.parentNode.insertBefore(s1,s0);
